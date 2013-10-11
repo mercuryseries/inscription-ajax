cedf4b557f9bea7cc4b6463a3225b88f687b2f96
Honoré HOUNWANOU <mercuryseries@gmail.com>
2013-10-11 22:16:23 +0200

    Ebauche de la page d'accueil

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>ESMT SOCIAL NETWORK</title>
+	<meta charset="UTF-8" />
+</head>
+<body>
+	<div id="main_wrapper">
+		<header>
+			<hgroup>
+				<h1>ESMT SOCIAL NETWORK</h1>
+				<h2>Bla bla</h2>
+			</hgroup>
+		</header>
+		
+		<div id="content">
+			<section id="presentation">
+			</section>
+			
+			<section id="inscription">
+			</section>
+			
+			<section id="connexion">
+			</section>
+		</div>
+		
+		<footer>
+			<p>&copy; Honoré HOUNWANOU - Tous droits réservés</p>
+		</footer>
+	</div>
+</body>
+</html>
